acb03f8d759a42a417823b72380553820998db1c
aasmah <35153854+aasmah@users.noreply.github.com>
2023-04-17 12:39:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,94 +1,157 @@
-import React, { useState } from "react";
-import {
-  Switch,
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Link
-} from "react-router-dom";
-import BaseForm from "./BaseForm";
-import BaseFormik from "./BaseFormik";
-import BaseFormikRefactored from "./BaseFormikRefactored";
-import FormikRenderProps from "./FormikRenderProps";
-import FormikRenderPropsRefactored from "./FormikRenderPropsRefactored";
-import WithFormik from "./WithFormik";
-import BaseFormikValidation from "./Validation/Form-Level-Validation/BaseFormikValidation";
-import BaseFormikYup from "./Validation/Form-Level-Validation/BaseFormikYup";
-import BaseFormikSetValues from "./BaseFormikSetValues";
 
-function HTMLForm() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-
-  return (
-    <form>
-      <div className="formRow">
-        <label htmlFor="email">Email address</label>
-        <input
-          type="email"
-          name="email"
-          className="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-      </div>
-      <div className="formRow">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          className="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <BaseForm />
-          </Route>
-          <Route path="/base-form">
-            <Redirect to="/" />
-          </Route>
-          <Route path="/base-formik">
-            <BaseFormik />
-          </Route>
-          <Route path="/base-formik-refactored">
-            <BaseFormikRefactored />
-          </Route>
-          <Route path="/formik-render-props">
-            <FormikRenderProps />
-          </Route>
-          <Route path="/formik-render-props-refactored">
-            <FormikRenderPropsRefactored />
-          </Route>
-          <Route path="/formik-render-props-refactored">
-            <FormikRenderPropsRefactored />
-          </Route>
-          <Route path="/validate">
-            <BaseFormikValidation />
-          </Route>
-          <Route path="/validate-schema">
-            <BaseFormikYup />
-          </Route>
-          <Route path="/with-formik">
-            <WithFormik />
-          </Route>
-          <Route path="/hatch-escape">
-            <BaseFormikSetValues />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+ 
+class App extends React.Component {
+  ...
+
+  render() {
+    const initialValues = {
+      fullname: '',
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      acceptTerms: false,
+    };
+
+    return (
+      <div className="register-form">
+        <Formik
+          initialValues={initialValues}
+          validationSchema={this.validationSchema}
+          onSubmit={this.handleSubmit}
+        >
+          {({ errors, touched, resetForm }) => (
+            <Form>
+              <div className="form-group">
+                <label>Full Name</label>
+                <Field
+                  name="fullname"
+                  type="text"
+                  className={
+                    'form-control' +
+                    (errors.fullname && touched.fullname ? ' is-invalid' : '')
+                  }
+                />
+                <ErrorMessage
+                  name="fullname"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="username"> Username </label>
+                <Field
+                  name="username"
+                  type="text"
+                  className={
+                    'form-control' +
+                    (errors.username && touched.username ? ' is-invalid' : '')
+                  }
+                />
+                <ErrorMessage
+                  name="username"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="email"> Email </label>
+                <Field
+                  name="email"
+                  type="email"
+                  className={
+                    'form-control' +
+                    (errors.email && touched.email ? ' is-invalid' : '')
+                  }
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password"> Password </label>
+                <Field
+                  name="password"
+                  type="password"
+                  className={
+                    'form-control' +
+                    (errors.password && touched.password ? ' is-invalid' : '')
+                  }
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="confirmPassword"> Confirm Password </label>
+                <Field
+                  name="confirmPassword"
+                  type="password"
+                  className={
+                    'form-control' +
+                    (errors.confirmPassword && touched.confirmPassword
+                      ? ' is-invalid'
+                      : '')
+                  }
+                />
+                <ErrorMessage
+                  name="confirmPassword"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
+
+              <div className="form-group form-check">
+                <Field
+                  name="acceptTerms"
+                  type="checkbox"
+                  className={
+                    'form-check-input' +
+                    (errors.acceptTerms && touched.acceptTerms
+                      ? ' is-invalid'
+                      : '')
+                  }
+                />
+                <label htmlFor="acceptTerms" className="form-check-label">
+                  I have read and agree to the Terms
+                </label>
+                <ErrorMessage
+                  name="acceptTerms"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
+
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary">
+                  Register
+                </button>
+                <button
+                  type="button"
+                  onClick={resetForm}
+                  className="btn btn-warning float-right"
+                >
+                  Reset
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    );
+  }
 }
 
 export default App;
